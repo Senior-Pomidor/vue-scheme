ca8a09b579d71816f2816523023962b972720fd7
Bobylev Vladimir <v.bobylev@rambler-co.ru>
2023-11-28 17:39:58 +0300

    feat: updated seat data format

--- src/components/scheme/SchemeSeat.vue
+++ src/components/scheme/SchemeSeat.vue
@@ -7,8 +7,8 @@
     <rect
       :width="props.seatWidth + 2"
       :height="props.seatHeight + 2"
-      :x="props.seat.coord_x"
-      :y="props.seat.coord_y"
+      :x="props.seat.x"
+      :y="props.seat.y"
       :rx="props.seat.styles?.rx || 3"
       :ry="props.seat.styles?.ry || 3"
       :fill="props.seat.styles?.bg_color || '#a99498'"
@@ -53,7 +53,7 @@
 
     <text
       color="black"
-      :transform="`translate(${props.seat.coord_x || 0} ${props.seat.coord_y || 0})`"
+      :transform="`translate(${props.seat.x || 0} ${props.seat.coord_y || 0})`"
     >
       <tspan
         v-if="seat.seat"
@@ -110,18 +110,18 @@
 
   const getSellChanelCoords = computed(() => ({
     is_api: {
-      cx: props.seat.coord_x + 3 || 0,
-      cy: props.seat.coord_y + 9 || 0,
+      cx: props.seat.x + 3 || 0,
+      cy: props.seat.y + 9 || 0,
     },
 
     widget: {
-      cx: props.seat.coord_x + 17 || 0,
-      cy: props.seat.coord_y + 17 || 0,
+      cx: props.seat.x + 17 || 0,
+      cy: props.seat.y + 17 || 0,
     },
 
     cashbox: {
-      cx: props.seat.coord_x + 3 || 0,
-      cy: props.seat.coord_y + 3 || 0,
+      cx: props.seat.x + 3 || 0,
+      cy: props.seat.y + 3 || 0,
     },
   }))
 
