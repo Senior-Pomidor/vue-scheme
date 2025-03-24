049b1d65a15010592303881e610e0f2cb2498a67
Bobylev Vladimir <v.bobylev@rambler-co.ru>
2025-03-24 12:21:35 +0300

    фильтрация только при необходимости

--- src/components/scheme/SchemeMain.vue
+++ src/components/scheme/SchemeMain.vue
@@ -54,92 +54,100 @@
   // локальная копия мест
   const localSeats = ref({})
 
-  const getSeatsProps = computed(() => props.seats)
+  // локальное хранилище отфильтрованных мест
+  const filteredSeats = ref({})
+
+  // функция фильтрации одного места
+  const filterSeat = (seat) => {
+    if (!Object.keys(props.filters).length) {
+      return true
+    }
+
+    for (const filter in props.filters) {
+      switch (filter) {
+        case 'attrs':
+          for (const attr in props.filters.attrs) {
+            if (Array.isArray(props.filters.attrs[attr])) {
+              if (props.filters.attrs[attr].length && !props.filters.attrs[attr].includes(seat[attr])) {
+                return false
+              }
+            } else if (typeof props.filters.attrs[attr] === 'object') {
+              let hasValidOption = false
+              for (const key in props.filters.attrs[attr]) {
+                if (JSON.stringify(props.filters.attrs[attr][key]) === JSON.stringify(seat[attr][key])) {
+                  hasValidOption = true
+                  break
+                }
+              }
+              if (!hasValidOption) return false
+            } else if (JSON.stringify(props.filters.attrs[attr]) !== JSON.stringify(seat[attr])) {
+              return false
+            }
+          }
+          break
+
+        case 'prices':
+          if (props.filters.prices.min && seat.price < props.filters.prices.min
+            || props.filters.prices.max && seat.price > props.filters.prices.max) {
+            return false
+          }
+          break
+        default:
+          break
+      }
+    }
+
+    return true
+  }
+
+  // фильтрация всех мест
+  const filterAllSeats = () => {
+    filteredSeats.value = {}
+    for (const id in localSeats.value) {
+      if (filterSeat(localSeats.value[id])) {
+        filteredSeats.value[id] = localSeats.value[id]
+      }
+    }
+  }
+
+  // фильтрация только измененных мест
+  const filterSeatsChunk = (seatsChunk) => {
+    for (const id in seatsChunk) {
+      if (filterSeat(seatsChunk[id])) {
+        filteredSeats.value[id] = seatsChunk[id]
+      } else {
+        delete filteredSeats.value[id]
+      }
+    }
+  }
 
   // наблюдение за обновлением всех мест
   watch(() => props.seats, (newSeats) => {
     localSeats.value = { ...newSeats }
+    filterAllSeats()
   }, { deep: true, immediate: true })
 
   // наблюдение за обновлением частичных мест
   watch(() => props.seatsChunk, (newSeatsChunk) => {
     if (!Object.keys(newSeatsChunk).length) return
 
-    // обновляем только указанные места с учетом фильтрации
+    // обновляем места в локальной копии
     localSeats.value = {
       ...localSeats.value,
       ...newSeatsChunk
     }
+
+    // фильтруем только измененные места
+    filterSeatsChunk(newSeatsChunk)
+  }, { deep: true })
+
+  // наблюдение за изменением фильтров
+  watch(() => props.filters, () => {
+    filterAllSeats()
   }, { deep: true })
 
   const getSeats = computed(() => localSeats.value)
-
-  const getQuotaSeats = computed(() => {
-    const seats = {}
-
-    for (const id in localSeats.value) {
-      const seat = localSeats.value[id]
-      let isValid = true
-
-
-      if (!Object.keys(props.filters).length) {
-        seats[id] = seat
-        continue
-      }
-
-      // HACK: временное решение для фильтров выделения
-      // FIXME: переписать этот говнокод при первой же возможности
-      for (const filter in props.filters) {
-        switch (filter) {
-          case 'attrs':
-            for (const attr in props.filters.attrs) {
-              if (Array.isArray(props.filters.attrs[attr])) {
-                if (props.filters.attrs[attr].length && !props.filters.attrs[attr].includes(seat[attr])) {
-                  isValid = false
-
-                  break
-                }
-              } else if (typeof props.filters.attrs[attr] === 'object') {
-                for (const key in props.filters.attrs[attr]) {
-
-                  // есть хоть один true вариант
-                  if (JSON.stringify(props.filters.attrs[attr][key]) === JSON.stringify(seat[attr][key])) {
-                    isValid = true
-
-                    break
-                  }
-
-                  isValid = false
-                }
-              } else if (JSON.stringify(props.filters.attrs[attr]) !== JSON.stringify(seat[attr])) {
-                isValid = false
-
-                break
-              }
-            }
-            break
-
-          case 'prices':
-            if (props.filters.prices.min && seat.price < props.filters.prices.min
-              || props.filters.prices.max && seat.price > props.filters.prices.max) {
-              isValid = false
-
-              break
-            }
-            break
-          default:
-            break
-        }
-      }
-
-      if (isValid) {
-        seats[id] = seat
-      }
-
-    }
-
-    return seats
-  })
+  const getQuotaSeats = computed(() => filteredSeats.value)
 
   const emit = defineEmits([
     'changedSeatsState',
