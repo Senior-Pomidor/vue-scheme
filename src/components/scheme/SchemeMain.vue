8f934c40395e164281ba469ef0a823aa8bd03906
Bobylev Vladimir <v.bobylev@rambler-co.ru>
2025-09-26 12:37:45 +0300

    feat(Scheme): clear selected seats

--- src/components/scheme/SchemeMain.vue
+++ src/components/scheme/SchemeMain.vue
@@ -42,6 +42,7 @@
   const seats = inject('schemeSeats')
   const seatsChunk = inject('schemeSeatsChunk')
   const isFullscreen = inject('isFullscreen', ref(false))
+  const hallSchemeApp = inject('hallSchemeApp')
 
   const actualSeats = ref({})
 
@@ -1233,6 +1234,58 @@
   }
   // END: зум для действий
 
+
+  // START: Сброс выделения мест
+  const clearSelectedSeatsHandler = () => {
+    const previouslySelectedIds = Object.keys(seatsState.value.selectedSeats)
+
+    if (!previouslySelectedIds.length) {
+      return
+    }
+
+    seatsState.value.selectedSeats = {}
+    currentSelectedSeats.value = {}
+    currentUnselectedSeats.value = {}
+
+    StateHistoryManager.saveState(seatsState.value)
+
+    objectsLayerRef.value?.getNode()?.clearCache()
+
+    if (offscreenCanvas.value && spatialIndex) {
+      previouslySelectedIds.forEach(id => {
+        const seat = actualSeats.value[id]
+
+        if (!seat) {
+          return
+        }
+
+        updateSnapshotArea(
+          seat,
+          actualSeats.value,
+          offscreenCanvas.value,
+          spatialIndex,
+          SEAT_SIZE,
+        )
+      })
+
+      snapshotImageRef.value?.getNode()?.image(offscreenCanvas.value)
+
+      snapshotImageRef.value?.getNode()?.getLayer()
+        ?.batchDraw()
+    } else {
+      createFullSnapshot()
+    }
+  }
+
+  const registerClearSelectedSeatsListener = () => {
+    if (!hallSchemeApp) {
+      return
+    }
+
+    hallSchemeApp.on(hallSchemeApp.events.clearSelectedSeats, clearSelectedSeatsHandler)
+  }
+  // END: Сброс выделения мест
+
   onMounted(() => {
     handleResize()
 
@@ -1242,6 +1295,8 @@
     activeZoomListenersAdd()
 
     addEventListenersForMousOverMode()
+
+    registerClearSelectedSeatsListener()
   })
 
   onBeforeUnmount(() => {
